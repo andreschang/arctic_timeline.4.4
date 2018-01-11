b0176d927b9bf12b2a2ccd4ed60358ce7be1accc
andreschang <andres_chang@brown.edu>
2018-01-11 00:56:57 -0500

    First commit

---
+++ sections.js
@@ -0,0 +1,792 @@
+
+/**
+ * scrollVis - encapsulates
+ * all the code for the visualization
+ * using reusable charts pattern:
+ * http://bost.ocks.org/mike/chart/
+ */
+var scrollVis = function () {
+  // constants to define the size
+  // and margins of the vis area.
+  var width = 500;
+  var height = 520;
+  var margin = { top: 0, left: 20, bottom: 40, right: 10 };
+
+  var lastIndex = -1;
+  var activeIndex = 0;
+
+
+  var svg = null;
+
+  // d3 selection that will be used
+  // for displaying visualizations
+  var g = null;
+
+  var activateFunctions = [];
+  var updateFunctions = [];
+
+  /**
+   * chart
+   *
+   * @param selection - the current d3 selection(s)
+   *  to draw the visualization in. For this
+   *  example, we will be drawing it in #vis
+   */
+  var chart = function (selection) {
+    selection.each(function (rawData) {
+      // create svg and give it a width and height
+      svg = d3.select(this).selectAll('svg').data([timelineData]);
+      var svgE = svg.enter().append('svg');
+      // @v4 use merge to combine enter and existing selection
+      svg = svg.merge(svgE);
+
+      svg.attr('width', width + margin.left + margin.right);
+      svg.attr('height', height + margin.top + margin.bottom);
+
+      svg.append('g');
+
+      // this group element will be used to contain all
+      // other elements.
+      g = svg.select('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+      // perform some preprocessing on raw data
+      var timelineData = getEvents(rawData);
+
+      setupVis(timelineData);
+
+      setupSections();
+    });
+  };
+
+  /**
+   * setupVis - creates initial elements for all
+   * sections of the visualization.
+   */
+
+  var setupVis = function (timelineData) {
+
+    // SLIDE 0
+    g.append('text')
+      .attr('class', 'title slide0')
+      .attr('y', (height / 5))
+      .attr('x', width / 3)
+      .text(timelineData[0].id)
+      .call(wrap, 450);
+
+    g.append('text')
+      .attr('class', 'desc slide0')
+      .attr('x', width / 5)
+      .attr('y', (height / 3) + (height / 5))
+      .text(timelineData[0].desc)
+      .call(wrap, 500);
+
+    g.selectAll('.slide0')
+      .attr('opacity', 0);
+
+    // SLIDE 1
+
+    g.append('svg:image')
+      .attr('class', 'slide1 img')
+      .attr('id', 'shuvinai')
+      .attr("xlink:href", "shuvinai.jpg")
+      .attr('x', 270)
+      .attr('y', 90)
+      .attr('width', '50%')
+      .attr('height', '50%');
+
+    g.append('text')
+      .attr('class', 'title slide1')
+      .attr('y', (height / 5))
+      .attr('x', width / 3)
+      .text(timelineData[1].id)
+      .call(wrap, 450);
+
+    g.append('text')
+      .attr('class', 'desc slide1')
+      .attr('x', width / 5)
+      .attr('y', (height / 3) + (height / 5))
+      .text(timelineData[1].desc)
+      .call(wrap, 500);
+
+    g.selectAll('.slide1')
+      .attr('opacity', 0);
+    g.selectAll('.slide1-img')
+      .attr('opacity', 0);
+
+    // SLIDE 2
+    g.append('text')
+      .attr('class', 'title slide2')
+      .attr('y', (height / 5))
+      .attr('x', width / 3)
+      .text(timelineData[2].id)
+      .call(wrap, 450);
+
+    g.append('text')
+      .attr('class', 'desc slide2')
+      .attr('x', width / 5)
+      .attr('y', (height / 3) + (height / 5))
+      .text(timelineData[2].desc)
+      .call(wrap, 500);
+
+    g.selectAll('.slide2')
+      .attr('opacity', 0);
+
+    // SLIDE 3
+    g.append('text')
+      .attr('class', 'title slide3')
+      .attr('y', (height / 5))
+      .attr('x', width / 3)
+      .text(timelineData[3].id)
+      .call(wrap, 450);
+
+    g.append('text')
+      .attr('class', 'desc slide3')
+      .attr('x', width / 5)
+      .attr('y', (height / 3) + (height / 5))
+      .text(timelineData[3].desc)
+      .call(wrap, 500);
+
+    g.selectAll('.slide3')
+      .attr('opacity', 0);
+
+    // SLIDE 4
+    g.append('text')
+      .attr('class', 'title slide4')
+      .attr('y', (height / 5))
+      .attr('x', width / 3)
+      .text(timelineData[4].id)
+      .call(wrap, 450);
+
+    g.append('text')
+      .attr('class', 'desc slide4')
+      .attr('x', width / 5)
+      .attr('y', (height / 3) + (height / 5))
+      .text(timelineData[4].desc)
+      .call(wrap, 500);
+
+    g.selectAll('.slide4')
+      .attr('opacity', 0);
+
+    // SLIDE 5
+    g.append('text')
+      .attr('class', 'title slide5')
+      .attr('y', (height / 5))
+      .attr('x', width / 3)
+      .text(timelineData[5].id)
+      .call(wrap, 450);
+
+    g.append('text')
+      .attr('class', 'desc slide5')
+      .attr('x', width / 5)
+      .attr('y', (height / 3) + (height / 5))
+      .text(timelineData[5].desc)
+      .call(wrap, 500);
+
+    g.selectAll('.slide5')
+      .attr('opacity', 0);
+
+    // SLIDE 6
+    g.append('text')
+      .attr('class', 'title slide6')
+      .attr('y', (height / 5))
+      .attr('x', width / 3)
+      .text(timelineData[6].id)
+      .call(wrap, 450);
+
+    g.append('text')
+      .attr('class', 'desc slide6')
+      .attr('x', width / 5)
+      .attr('y', (height / 3) + (height / 5))
+      .text(timelineData[6].desc)
+      .call(wrap, 500);
+
+    g.selectAll('.slide6')
+      .attr('opacity', 0);
+
+    // SLIDE 7
+    g.append('text')
+      .attr('class', 'title slide7')
+      .attr('y', (height / 5))
+      .attr('x', width / 3)
+      .text(timelineData[7].id)
+      .call(wrap, 450);
+
+    g.append('text')
+      .attr('class', 'desc slide7')
+      .attr('x', width / 5)
+      .attr('y', (height / 3) + (height / 5))
+      .text(timelineData[7].desc)
+      .call(wrap, 500);
+
+    g.selectAll('.slide7')
+      .attr('opacity', 0);
+  
+    // SLIDE 8
+    g.append('text')
+      .attr('class', 'title slide8')
+      .attr('y', (height / 5))
+      .attr('x', width / 3)
+      .text(timelineData[8].id)
+      .call(wrap, 450);
+
+    g.append('text')
+      .attr('class', 'desc slide8')
+      .attr('x', width / 5)
+      .attr('y', (height / 3) + (height / 5))
+      .text(timelineData[8].desc)
+      .call(wrap, 500);
+
+    g.selectAll('.slide8')
+      .attr('opacity', 0);
+
+    // SLIDE 9
+    g.append('text')
+      .attr('class', 'title slide9')
+      .attr('y', (height / 5))
+      .attr('x', width / 3)
+      .text(timelineData[9].id)
+      .call(wrap, 450);
+
+    g.append('text')
+      .attr('class', 'desc slide9')
+      .attr('x', width / 5)
+      .attr('y', (height / 3) + (height / 5))
+      .text(timelineData[9].desc)
+      .call(wrap, 500);
+
+    g.selectAll('.slide9')
+      .attr('opacity', 0);
+
+    // SLIDE 10
+    g.append('text')
+      .attr('class', 'title slide10')
+      .attr('y', (height / 5))
+      .attr('x', width / 3)
+      .text(timelineData[10].id)
+      .call(wrap, 450);
+
+    g.append('text')
+      .attr('class', 'desc slide10')
+      .attr('x', width / 5)
+      .attr('y', (height / 3) + (height / 5))
+      .text(timelineData[10].desc)
+      .call(wrap, 500);
+
+    g.selectAll('.slide10')
+      .attr('opacity', 0);
+
+    // SLIDE 11
+    g.append('text')
+      .attr('class', 'title slide11')
+      .attr('y', (height / 5))
+      .attr('x', width / 3)
+      .text(timelineData[11].id)
+      .call(wrap, 450);
+
+    g.append('text')
+      .attr('class', 'desc slide11')
+      .attr('x', width / 5)
+      .attr('y', (height / 3) + (height / 5))
+      .text(timelineData[11].desc)
+      .call(wrap, 500);
+
+    g.selectAll('.slide11')
+      .attr('opacity', 0);
+
+    // SLIDE 12
+    g.append('text')
+      .attr('class', 'title slide12')
+      .attr('y', (height / 5))
+      .attr('x', width / 3)
+      .text(timelineData[12].id)
+      .call(wrap, 450);
+
+    g.append('text')
+      .attr('class', 'desc slide12')
+      .attr('x', width / 5)
+      .attr('y', (height / 3) + (height / 5))
+      .text(timelineData[12].desc)
+      .call(wrap, 500);
+
+    g.selectAll('.slide12')
+      .attr('opacity', 0);
+  
+    // SLIDE 13
+    g.append('text')
+      .attr('class', 'title slide13')
+      .attr('y', (height / 5))
+      .attr('x', width / 3)
+      .text(timelineData[13].id)
+      .call(wrap, 450);
+
+    g.append('text')
+      .attr('class', 'desc slide13')
+      .attr('x', width / 5)
+      .attr('y', (height / 3) + (height / 5))
+      .text(timelineData[13].desc)
+      .call(wrap, 500);
+
+    g.selectAll('.slide13')
+      .attr('opacity', 0);
+
+    // SLIDE 14
+    g.append('text')
+      .attr('class', 'title slide14')
+      .attr('y', (height / 5))
+      .attr('x', width / 3)
+      .text(timelineData[14].id)
+      .call(wrap, 450);
+
+    g.append('text')
+      .attr('class', 'desc slide14')
+      .attr('x', width / 5)
+      .attr('y', (height / 3) + (height / 5))
+      .text(timelineData[14].desc)
+      .call(wrap, 500);
+
+    g.selectAll('.slide14')
+      .attr('opacity', 0);
+    };
+
+  var setupSections = function () {
+    // activateFunctions are called each
+    // time the active section changes
+    activateFunctions[0] = show0;
+    activateFunctions[1] = show1;
+    activateFunctions[2] = show2;
+    activateFunctions[3] = show3;
+    activateFunctions[4] = show4;
+    activateFunctions[5] = show5;
+    activateFunctions[6] = show6;
+    activateFunctions[7] = show7;
+    activateFunctions[8] = show8;
+    activateFunctions[9] = show9;
+    activateFunctions[10] = show10;
+    activateFunctions[11] = show11;
+    activateFunctions[12] = show12;
+    activateFunctions[13] = show13;
+    activateFunctions[14] = show14;
+
+    // updateFunctions are called while
+    // in a particular section to update
+    // the scroll progress in that section.
+    // Most sections do not need to be updated
+    // for all scrolling and so are set to
+    // no-op functions.
+    for (var i = 0; i < 9; i++) {
+      updateFunctions[i] = function () {};
+    }
+  };
+
+  var wrap = function(text, width) {
+  text.each(function() {
+    var text = d3.select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        y = text.attr("y"),
+        // dy = parseFloat(text.attr("dy")),
+        dy = 0,
+        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+    while (word = words.pop()) {
+      line.push(word);
+      tspan.text(line.join(" "));
+      if (tspan.node().getComputedTextLength() > width) {
+        line.pop();
+        tspan.text(line.join(" "));
+        line = [word];
+        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+      }
+    }
+  });
+}
+
+  /**
+   * ACTIVATE FUNCTIONS
+   *
+   * These will be called their
+   * section is scrolled to.
+   *
+   * General pattern is to ensure
+   * all content for the current section
+   * is transitioned in, while hiding
+   * the content for the previous section
+   * as well as the next section (as the
+   * user may be scrolling up or down).
+   *
+   */
+
+  function show0() {
+    g.selectAll('.slide1')
+      .transition()
+      .duration(0)
+      .attr('opacity', 0);
+
+    g.selectAll('.slide0')
+      .transition()
+      .duration(600)
+      .attr('opacity', 1.0);
+  }
+
+  function show1() {
+    g.selectAll('.slide0')
+      .transition()
+      .duration(0)
+      .attr('opacity', 0);
+
+    g.selectAll('.slide2')
+      .transition()
+      .duration(0)
+      .attr('opacity', 0);
+
+    g.selectAll('.slide1')
+      .transition()
+      .duration(600)
+      .attr('opacity', 1.0);
+
+    g.selectAll('.slide1').filter('.img')
+      .transition()
+      .duration(600)
+      .attr('opacity', 0.4);
+      // .on("mouseover", handleMouseOver)
+      // .on("mouseout", handleMouseOut);
+
+  }
+
+  function show2() {
+    g.selectAll('.slide1')
+      .transition()
+      .duration(0)
+      .attr('opacity', 0);
+
+    g.selectAll('.slide2')
+      .transition()
+      .duration(600)
+      .attr('opacity', 1.0);
+
+    g.selectAll('.slide3')
+      .transition()
+      .duration(600)
+      .attr('opacity', 0);
+  }
+
+  function show3() {
+    g.selectAll('.slide2')
+      .transition()
+      .duration(0)
+      .attr('opacity', 0);
+
+    g.selectAll('.slide3')
+      .transition()
+      .duration(600)
+      .attr('opacity', 1.0);
+
+    g.selectAll('.slide4')
+      .transition()
+      .duration(600)
+      .attr('opacity', 0);
+  }
+
+  function show4() {
+    g.selectAll('.slide3')
+      .transition()
+      .duration(0)
+      .attr('opacity', 0);
+
+    g.selectAll('.slide4')
+      .transition()
+      .duration(600)
+      .attr('opacity', 1.0);
+
+    g.selectAll('.slide5')
+      .transition()
+      .duration(600)
+      .attr('opacity', 0);
+  }
+
+  function show5() {
+    g.selectAll('.slide4')
+      .transition()
+      .duration(0)
+      .attr('opacity', 0);
+
+    g.selectAll('.slide5')
+      .transition()
+      .duration(600)
+      .attr('opacity', 1.0);
+
+    g.selectAll('.slide6')
+      .transition()
+      .duration(600)
+      .attr('opacity', 0.0);
+
+  }
+
+  function show6() {
+    g.selectAll('.slide5')
+      .transition()
+      .duration(0)
+      .attr('opacity', 0);
+
+    g.selectAll('.slide6')
+      .transition()
+      .duration(600)
+      .attr('opacity', 1.0);
+
+    g.selectAll('.slide7')
+      .transition()
+      .duration(600)
+      .attr('opacity', 0.0);
+
+  }
+
+  function show7() {
+    g.selectAll('.slide6')
+      .transition()
+      .duration(0)
+      .attr('opacity', 0);
+
+    g.selectAll('.slide7')
+      .transition()
+      .duration(600)
+      .attr('opacity', 1.0);
+
+    g.selectAll('.slide8')
+      .transition()
+      .duration(600)
+      .attr('opacity', 0.0);
+
+  }
+
+  function show8() {
+    g.selectAll('.slide7')
+      .transition()
+      .duration(0)
+      .attr('opacity', 0);
+
+    g.selectAll('.slide8')
+      .transition()
+      .duration(600)
+      .attr('opacity', 1.0);
+
+    g.selectAll('.slide9')
+      .transition()
+      .duration(600)
+      .attr('opacity', 0.0);
+
+    // d3.select('#miniBox')
+    //   .attr('height', 20);
+
+  }
+
+  function show9() {
+    g.selectAll('.slide8')
+      .transition()
+      .duration(0)
+      .attr('opacity', 0);
+
+    g.selectAll('.slide9')
+      .transition()
+      .duration(600)
+      .attr('opacity', 1.0);
+
+    g.selectAll('.slide10')
+      .transition()
+      .duration(600)
+      .attr('opacity', 0.0);
+
+  }
+
+  function show10() {
+    g.selectAll('.slide9')
+      .transition()
+      .duration(0)
+      .attr('opacity', 0);
+
+    g.selectAll('.slide10')
+      .transition()
+      .duration(600)
+      .attr('opacity', 1.0);
+
+    g.selectAll('.slide11')
+      .transition()
+      .duration(600)
+      .attr('opacity', 0.0);
+
+  }
+
+  function show11() {
+    g.selectAll('.slide10')
+      .transition()
+      .duration(0)
+      .attr('opacity', 0);
+
+    g.selectAll('.slide11')
+      .transition()
+      .duration(600)
+      .attr('opacity', 1.0);
+
+    g.selectAll('.slide12')
+      .transition()
+      .duration(600)
+      .attr('opacity', 0.0);
+
+  }
+
+  function show12() {
+    g.selectAll('.slide11')
+      .transition()
+      .duration(0)
+      .attr('opacity', 0);
+
+    g.selectAll('.slide12')
+      .transition()
+      .duration(600)
+      .attr('opacity', 1.0);
+
+    g.selectAll('.slide13')
+      .transition()
+      .duration(600)
+      .attr('opacity', 0.0);
+
+  }
+
+  function show13() {
+    g.selectAll('.slide12')
+      .transition()
+      .duration(0)
+      .attr('opacity', 0);
+
+    g.selectAll('.slide13')
+      .transition()
+      .duration(600)
+      .attr('opacity', 1.0);
+
+    g.selectAll('.slide14')
+      .transition()
+      .duration(600)
+      .attr('opacity', 0);
+
+  }
+
+function show14() {
+    g.selectAll('.slide13')
+      .transition()
+      .duration(0)
+      .attr('opacity', 0);
+
+    g.selectAll('.slide14')
+      .transition()
+      .duration(600)
+      .attr('opacity', 1.0);
+
+  }
+
+// Add interactivity 
+function handleMouseOver(d, i) {
+  var selectID = this.getAttribute('id');
+  d3.select("#"+selectID)
+    .transition()
+    .duration(200)
+    .style("opacity", 1.0);
+};
+
+function handleMouseOut(d, i) {
+  var selectID = this.getAttribute('id');
+  d3.select("#"+selectID)
+    .transition()
+    .duration(200)
+    .style("opacity", 0.4);
+};
+
+
+  /**
+   * DATA FUNCTIONS
+   *
+   * Used to coerce the data into the
+   * formats we need to visualize
+   *
+   */
+
+  function getEvents(rawData) {
+    return rawData.map(function (d,i) {
+      d.lane = d.lane;
+      d.start = d.start;
+      d.end = (d.end === '') ? 10 : d.end;
+      d.desc = d.desc;
+      return d;
+    });
+  }
+
+
+  chart.activate = function (index) {
+    activeIndex = index;
+    var sign = (activeIndex - lastIndex) < 0 ? -1 : 1;
+    var scrolledSections = d3.range(lastIndex + sign, activeIndex + sign, sign);
+    scrolledSections.forEach(function (i) {
+      activateFunctions[i]();
+    });
+    lastIndex = activeIndex;
+  };
+
+  /**
+   * update
+   *
+   * @param index
+   * @param progress
+   */
+  chart.update = function (index, progress) {
+    updateFunctions[index](progress);
+  };
+
+  // return chart function
+  return chart;
+};
+
+
+/**
+ * display - called once data
+ * has been loaded.
+ * sets up the scroller and
+ * displays the visualization.
+ *
+ * @param data - loaded tsv data
+ */
+function display(data) {
+  // create a new plot and
+  // display it
+  var plot = scrollVis();
+  d3.select('#vis')
+    .datum(data)
+    .call(plot);
+
+  // setup scroll functionality
+  var scroll = scroller()
+    .container(d3.select('#graphic'));
+
+  // pass in .step selection as the steps
+  scroll(d3.selectAll('.step'));
+
+  // setup event handling
+  scroll.on('active', function (index) {
+    // highlight current step text
+    d3.selectAll('.step')
+      .style('opacity', function (d, i) { return i === index ? 1 : 0.1; });
+
+    // activate current section
+    plot.activate(index);
+  });
+
+  scroll.on('progress', function (index, progress) {
+    plot.update(index, progress);
+  });
+}
+
+// load data and display
+// d3.tsv('web_timeline4.2.tsv', display);
+d3.tsv('https://rawgit.com/andreschang/arctic_timeline.4.2/master/web_timeline4.2.tsv', display);
+
+
---
+++ timeline.4.2.1.js
@@ -0,0 +1,234 @@
+// Version 4.2.1
+// 2018-01-08
+// Plays nicely with scroll
+// Builds mini-timeline on left
+// And attaches 'step' attributes 
+// 2018-01-09
+// Adds locator box to miniTL
+// 2018-01-10
+// Polished locator box
+// Added v2 panel text
+// Added image in Slide1
+// Need to get mouseover to work
+
+// Parameters
+var lanes = ["Timeline"],
+  laneLength = lanes.length,
+  timeBegin = -1000000,
+  timeEnd = 2020;
+
+var tf = [2020, 1800, 0, -22000, -100000],
+  t0 = [1801, 1, -21999, -99999, -999999],
+  units = [10, 100, 1000, 10000, 100000],
+  nUnits = tf.map(function(n, i) { return (tf[i]+1-t0[i]) / units[i]; });
+
+tf.splice(5,0,-1000000)
+
+// Placement
+var mTop = 50,
+  m = 15,
+  mainHeight = 6000 - mTop-m,
+  miniHeight = 500 - mTop-m,
+  miniWidth = 100 - 2*m,
+  mainWidth = 350 - miniWidth;
+
+// Calculate ranges for shifting scale timeline
+nUnits.splice(0,0,0);
+var nDom = nUnits.map( function(n, i) { return nUnits.slice(0,i+1).reduce(getSum) ;}),
+  totalDom = nUnits.reduce(getSum),
+  scaleDom = nDom.map( function(n) { return n*mainHeight/totalDom});
+
+var scale0 = d3.scaleLinear().domain([tf[0], tf[1]]).range([scaleDom[0],scaleDom[1]]),
+  scale1 = d3.scaleLinear().domain([tf[1], tf[2]]).range([scaleDom[1],scaleDom[2]]),
+  scale2 = d3.scaleLinear().domain([tf[2], tf[3]]).range([scaleDom[2],scaleDom[3]]),
+  scale3 = d3.scaleLinear().domain([tf[3], tf[4]]).range([scaleDom[3],scaleDom[4]]),
+  scale4 = d3.scaleLinear().domain([tf[4], tf[5]]).range([scaleDom[4],scaleDom[5]]);
+
+var y2 = d3.scaleLinear()
+  .domain([timeEnd, tf[5]])
+  .range([0, miniHeight]);
+var x1 = d3.scaleLinear()
+  .domain([0, laneLength])
+  .range([0, mainWidth]);
+var x2 = d3.scaleLinear()
+  .domain([0, laneLength])
+  .range([0, miniWidth]);
+
+function multiScale(inputNumber) {
+      if (tf[1] <= inputNumber && inputNumber <= tf[0]) {
+        return scale0(inputNumber);
+      } else if (tf[2] <= inputNumber && inputNumber < tf[1]) {
+        return scale1(inputNumber);
+      } else if (tf[3] <= inputNumber && inputNumber < tf[2]) {
+        return scale2(inputNumber);
+      } else if (tf[4] <= inputNumber && inputNumber < tf[3]) {
+        return scale3(inputNumber);
+      } else if (tf[5] <= inputNumber && inputNumber < tf[4]) {
+        return scale4(inputNumber);
+      };
+};
+
+var bounds = tf.map(function(n) {return multiScale(n) ;} )
+
+function boxMultiScale(inputNumber) {
+      if (bounds[0] <= inputNumber && inputNumber <= bounds[1]) {
+        return y2(scale0.invert(inputNumber));
+      } else if (bounds[1] <= inputNumber && inputNumber < bounds[2]) {
+        return y2(scale1.invert(inputNumber));
+      } else if (bounds[2] <= inputNumber && inputNumber < bounds[3]) {
+        return y2(scale2.invert(inputNumber));
+      } else if (bounds[3] <= inputNumber && inputNumber < bounds[4]) {
+        return y2(scale3.invert(inputNumber));
+      } else if (inputNumber < bounds[5]) {
+        return y2(scale4.invert(inputNumber));
+      };
+};
+
+// }
+
+function getSum(total, num) {
+    return total + num;
+};
+
+// BUILD MAIN TL
+// main timeline
+var mainTL = d3.select("#sections")
+  .append("svg")
+  .attr("width", mainWidth+miniWidth)
+  .attr("height", mainHeight+mTop+m)
+  .append("g")
+  .attr("transform", "translate(" + (miniWidth) + "," + mTop + ")") // position mainTL
+  .attr("height", mainHeight)
+  .attr("width", mainWidth);
+
+mainTL.append("rect")
+  .attr("x", .12*x1(1))
+  .attr("width", function(d) {return .8*x1(1)})
+  .attr("height", mainHeight)
+  .attr("fill", "#edf1f2");
+
+// BUILD EACH Y-SCALE
+var axis0 = d3.axisLeft(scale0)
+  .ticks(20, "f");
+var axis1 = d3.axisLeft(scale1)
+  .ticks(20, "f");
+var axis2 = d3.axisLeft(scale2)
+  .ticks(20, "f");
+var axis3 = d3.axisLeft(scale3)
+  // .ticks(20, ".2s");
+  .ticks(10, "f");
+var axis4 = d3.axisLeft(scale4)
+  // .ticks(20, ".2s");
+  .ticks(10, "f");
+
+// minor ticks
+// var axis2m = d3.axisLeft(scale2)
+//   .ticks(2000, "f");
+// var axis1m = d3.axisLeft(scale1)
+//   .ticks(200, "f");
+
+// var yAxis = d3.axisLeft(y1);
+
+// BUILD MINI TL
+// mini timeline
+var miniTL = d3.select("#miniTL")
+  .append("svg")
+  .attr("width", miniWidth+2*m)
+  .attr("height", miniHeight+mTop+m)
+  .append("g")
+  .attr("transform", "translate(" + m + "," + mTop + ")") // position miniTL
+  .attr("height", miniHeight)
+  .attr("width", miniWidth);
+
+miniTL.append("rect")
+  .attr("x", 0.05*x2(1))
+  .attr("width", function(d) {return .8*x2(1)})
+  // height edited to match limit of miniBox slider
+  .attr("height", (miniHeight-16))
+  .attr("fill", "#edf1f2");
+
+// Load data and build timelines
+// d3.tsv("web_timeline4.2.tsv", function(items) {
+d3.tsv("https://rawgit.com/andreschang/arctic_timeline.4.2/master/web_timeline4.2.tsv", function(items) {
+
+  // lines
+
+  // main events
+  mainTL.append("g").selectAll("mainEvent")
+    .data(items)
+    .enter().append("rect")
+    .attr("class", function(d) {return "event" + d.lane + " mainEvent" + " event" + d.lane + d.start + " step"})
+    .attr("id", function(d) {return "mainEvent"+ d.lane + d.start})
+    .attr("y", function(d) {return multiScale(d.end, tf, t0, scaleDom);})
+    .attr("x", .12*x1(1))
+    .attr("width", function(d) {return .8*x1(1)})
+    .attr("height", function(d) {return multiScale(d.start, tf, t0, scaleDom)-multiScale(d.end, tf, t0, scaleDom)});
+
+  // append y-axes
+  mainTL.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(20,0)")
+    .call(axis0)
+    .select(".domain").remove();
+
+  mainTL.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(20,0)")
+    .call(axis1)
+    .select(".domain").remove();
+
+  mainTL.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(20,0)")
+    .call(axis2)
+    .select(".domain").remove();
+
+  mainTL.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(20,0)")
+    .call(axis3)
+    .select(".domain").remove();
+
+  mainTL.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(20,0)")
+    .call(axis4)
+    .select(".domain").remove();
+
+
+  // minor ticks
+  // mainTL.append("g")
+  //   .attr("class", "axis")
+  //   .attr("transform", "translate(20,0)")
+  //   .call(axis1m)
+  //   .selectAll("text, .domain").remove();
+
+  // mainTL.append("g")
+  //   .attr("class", "axis")
+  //   .attr("transform", "translate(20,0)")
+  //   .call(axis2m)
+  //   .selectAll("text, .domain").remove();
+
+  // //mini events
+  miniTL.append("g").selectAll("miniEvent")
+    .data(items)
+    .enter().append("rect")
+    .attr("class", function(d) {return "event" + d.lane + " miniEvent" + " event" + d.lane + d.start})
+    .attr("y", function(d) {return y2(d.end);})
+    // .attr("x", function(d) {return x2(d.lane)+0.1*x2(1);})
+    .attr("x", 0.05*x2(1))
+    .attr("width", function(d) {return .8*x2(1)})
+    .attr("height", function(d) {return y2(d.start)-y2(d.end)});
+
+  miniTL.append("rect")
+    .attr("id", "miniBox")
+    .attr("width", function(d) {return .9*x2(1)})
+    .attr("height", 10)
+    .attr("fill", "purple")
+    .attr("opacity", .3)
+    .attr();
+
+  d3.selectAll(".event0-1000000").filter(".miniEvent")
+    .attr("height", (86.8246-16));
+
+ })
